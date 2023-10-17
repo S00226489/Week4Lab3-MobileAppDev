78cc5cc95b558f015bff1960735c6148e4687c91
AiFar <s00226489@mail.itsligo.ie>
2023-10-17 15:47:25 +0100

    trying to get landscape working

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lab3;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,24 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set click listeners for dance style buttons
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayToast("Samba");
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayToast("Rock 'n' Roll");
+            }
+        });
+
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayToast("Hip Hop");
+            }
+        });
     }
 
-    public void displayToast(View view) {
-        String danceType = "";
-        int viewId = view.getId();
-
-        if (viewId == R.id.button3) {
-            danceType = "Samba";
-        } else if (viewId == R.id.button2) {
-            danceType = "Rock 'n' Roll";
-        } else if (viewId == R.id.button1) {
-            danceType = "Hip Hop";
-        }
-
-        if (!danceType.isEmpty()) {
-            String toastMessage = "You chose: " + danceType;
-            Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
-        }
+    private void displayToast(String danceType) {
+        String toastMessage = "You chose: " + danceType;
+        Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
     }
 }
-
